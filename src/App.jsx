71d9e6ddefa072630ba5992f65a7ea72af26d7d9
sahle-1996 <sahlesiumgoitom@gmai.com>
2024-11-24 23:51:12 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Card from './Card.jsx';
-import './index.css'; // For the card styles
-import htmlPic from './assets/HTML.jpeg'; // Image for the first card
-import cssPic from './assets/CSS.jpeg'; // Image for the second card
-import jsPic from './assets/JavaScript.jpeg'; // Image for the third card
+import './index.css'; 
+import htmlPic from './assets/HTML.jpeg'; 
+import cssPic from './assets/CSS.jpeg';
+import jsPic from './assets/JavaScript.jpeg';
 
 function App() {
   return (
