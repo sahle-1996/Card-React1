0ea55fe0f86ef79cf1dbade44a53097b415f6151
sahle-1996 <sahlesiumgoitom@gmai.com>
2024-11-25 11:30:39 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import CardList from './CardList.jsx'; 
 import './index.css'; 
 
@@ -9,12 +10,12 @@ const cardData = [
     description: 'Some of the best HTML courses available.',
   },
   {
-    image: './src/assets/CSS.jpeg', 
+    image: './assets/CSS.jpeg', 
     title: 'CSS Course',
     description: 'Learn styling with CSS in these great courses.',
   },
   {
-    image: './src/assets/JavaScript.jpeg', 
+    image: './assets/JavaScript.jpeg', 
     title: 'JavaScript Course',
     description: 'Master JavaScript with these amazing tutorials.',
   },
@@ -24,7 +25,7 @@ function App() {
   return (
     <div>
       <h1 style={{ textAlign: 'center', margin: '20px 0' }}>My React Cards</h1>
-      <CardList cards={cardData} /> {/* Pass the card data as props */}
+      <CardList cards={cardData} /> 
     </div>
   );
 }
